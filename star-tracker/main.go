7a1bb88f37f15d40ccacd627ccb86ae7f4e1fc6b
Alexander J Trelore <alextrelore@gmail.com>
2018-10-10 22:18:06 +0100

    Finished star-tracker proof of concept

--- star-tracker/main.go
+++ star-tracker/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/gocarina/gocsv"
 )
 
 const inDir = "/pfs/statsapi/api/v1/game/"
@@ -34,10 +37,34 @@ type Star struct {
 	Link     string `json:"link"`
 }
 
+// this really wants to be a csv like
+// ID, Link, FullName, FirstStars, SecondStars, ThirdStars
+// 8477956, /api/v1/people/8477956, David Pastrnak, 0, 0, 1
+
+// Output is the csv file we ammend to track star of the game players
+type Output struct {
+	Stars []StarCount
+}
+
+// StarCount has the info of how many times that player got that # star of the game
+type StarCount struct {
+	ID         int
+	Link       string
+	FullName   string
+	FirstStar  int
+	SecondStar int
+	ThirdStar  int
+}
+
 func main() {
 	os.Mkdir(outDir, 0700)
 
-	log.Printf("Reading directory %s\n", inDir)
+	stars, err := extractDataFromCSVFile(outDir + "stars.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Reading dir  %s\n", inDir)
 
 	dir, err := ioutil.ReadDir(inDir)
 	if err != nil {
@@ -46,14 +73,76 @@ func main() {
 
 	for _, d := range dir {
 		fileName := inDir + d.Name() + "/feed/live"
-		err := readFile(fileName)
+		var liveFeed LiveFeedData
+		err := extractDataFromJSONFile(&liveFeed, fileName)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// inefficient just wanting to get this working to move onto more meaningful transformations
+		// also a defect here, needs splitting out into a func and testing independently
+		foundFirst, foundSecond, foundThird := false, false, false
+		for _, star := range stars {
+			if star.ID == liveFeed.LiveData.Decisions.FirstStar.ID {
+				star.FirstStar++
+				foundFirst = true
+			}
+			if star.ID == liveFeed.LiveData.Decisions.SecondStar.ID {
+				star.SecondStar++
+				foundSecond = true
+			}
+			if star.ID == liveFeed.LiveData.Decisions.ThirdStar.ID {
+				star.ThirdStar++
+				foundThird = true
+			}
+		}
+		if !foundFirst {
+			stars = append(stars, StarCount{
+				ID:         liveFeed.LiveData.Decisions.FirstStar.ID,
+				Link:       liveFeed.LiveData.Decisions.FirstStar.Link,
+				FullName:   liveFeed.LiveData.Decisions.FirstStar.FullName,
+				FirstStar:  1,
+				SecondStar: 0,
+				ThirdStar:  0,
+			})
+		}
+		if !foundSecond {
+			stars = append(stars, StarCount{
+				ID:         liveFeed.LiveData.Decisions.SecondStar.ID,
+				Link:       liveFeed.LiveData.Decisions.SecondStar.Link,
+				FullName:   liveFeed.LiveData.Decisions.SecondStar.FullName,
+				FirstStar:  0,
+				SecondStar: 1,
+				ThirdStar:  0,
+			})
+		}
+		if !foundThird {
+			stars = append(stars, StarCount{
+				ID:         liveFeed.LiveData.Decisions.ThirdStar.ID,
+				Link:       liveFeed.LiveData.Decisions.ThirdStar.Link,
+				FullName:   liveFeed.LiveData.Decisions.ThirdStar.FullName,
+				FirstStar:  0,
+				SecondStar: 0,
+				ThirdStar:  1,
+			})
+		}
+	}
+	//write stars back out to file
+	f, err := os.OpenFile(outDir+"stars.csv", os.O_RDWR, os.ModeExclusive)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = gocsv.MarshalCSVWithoutHeaders(stars, gocsv.NewSafeCSVWriter(csv.NewWriter(f)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func readFile(src string) error {
+func extractDataFromJSONFile(v interface{}, src string) error {
 	log.Printf("Reading %s\n", src)
 	source, err := os.Open(src)
 	if err != nil {
@@ -61,11 +150,32 @@ func readFile(src string) error {
 	}
 	defer source.Close()
 
-	var liveFeed LiveFeedData
-	if err := json.NewDecoder(source).Decode(&liveFeed); err != nil {
+	if err := json.NewDecoder(source).Decode(&v); err != nil {
 		return err
 	}
-
-	log.Println(liveFeed)
 	return nil
+}
+
+func extractDataFromCSVFile(src string) ([]StarCount, error) {
+	log.Printf("Reading %s\n", src)
+	source, err := os.Create(src)
+	if err != nil {
+		return nil, err
+	}
+	defer source.Close()
+
+	var stars []StarCount
+
+	f, err := source.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if f.Size() == 0 {
+		return stars, nil
+	}
+
+	if err := gocsv.UnmarshalWithoutHeaders(source, &stars); err != nil {
+		return nil, err
+	}
+	return stars, nil
 }
--- star-tracker/main_test.go
+++ star-tracker/main_test.go
@@ -1,7 +1,72 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestReadFile(t *testing.T) {
+func TestAddStars(t *testing.T) {
+	td := []struct {
+		name           string
+		output         Output
+		decisions      Decisions
+		expectedOutput Output
+	}{
+		{
+			name:   "Add stars to empty output",
+			output: Output{},
+			decisions: Decisions{
+				FirstStar: Star{
+					ID:       8470860,
+					FullName: "Jaroslav Halak",
+					Link:     "/api/v1/people/8470860",
+				},
+				SecondStar: Star{
+					ID:       8473419,
+					FullName: "Brad Marchand",
+					Link:     "/api/v1/people/8473419",
+				},
+				ThirdStar: Star{
+					ID:       8477956,
+					FullName: "David Pastrnak",
+					Link:     "/api/v1/people/8477956",
+				},
+			},
+			expectedOutput: Output{
+				FirstStars: []StarCount{
+					StarCount{
+						ID:       8470860,
+						FullName: "Jaroslav Halak",
+						Link:     "/api/v1/people/8470860",
+						Count:    1,
+					},
+				},
+				SecondStars: []StarCount{
+					StarCount{
+						ID:       8473419,
+						FullName: "Brad Marchand",
+						Link:     "/api/v1/people/8473419",
+						Count:    1,
+					},
+				},
+				ThirdStars: []StarCount{
+					StarCount{
+						ID:       8477956,
+						FullName: "David Pastrnak",
+						Link:     "/api/v1/people/8477956",
+						Count:    1,
+					},
+				},
+			},
+		},
+	}
 
+	for _, tc := range td {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.output.addStars(tc.decisions)
+			if !reflect.DeepEqual(tc.output, tc.expectedOutput) {
+				t.Errorf("expected %v\ngot %v", tc.expectedOutput, tc.output)
+			}
+		})
+	}
 }
