98f5657bba7d2f790538bb8c7680d4cf9e6ceab8
Alexander J Trelore <alextrelore@gmail.com>
2018-11-03 16:44:17 +0000

    Simplify response struct

--- game-drawerv2/build_save.sh
+++ game-drawerv2/build_save.sh
@@ -12,5 +12,6 @@ docker save -o bin/game-drawerv2-pachd.img game-drawerv2:0.0.0.$ver | gzip -c > 
 # updating game-drawerv2 to use docker image
 sed "s/VERSION/0.0.0.$ver/g" game-drawerv2.jsonnet.tmpl > game-drawerv2.jsonnet
 
+rm -rf pipelines
 mkdir pipelines
 jsonnet -m . game-drawerv2.jsonnet
